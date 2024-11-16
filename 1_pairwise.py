a01f5be1f60a51da4b256802f6b2c8c4dee2740b
Paulo H. <paulohtolotti@alumni.usp.br>
2024-11-16 17:36:11 -0300

    first commit:maximum pairwise product

---
+++ 1_pairwise.py
@@ -0,0 +1,11 @@
+# Solução gambiarra minha
+arr = [1, 4, 3, 6, 7, 0]
+max_el = [0, 0]
+max = 0
+for i in range(len(arr)-2):
+    for j in arr:
+        if (arr[i]*j > max):
+            max = arr[i]*j
+            max_el[0] = arr[i]
+            max_el[1] = j
+print(f"Maximum product is {max}, and is produced by the elements {max_el}")
